c03813e1d2191998f0b1af8bb2fc94b4f4d0ece7
retater56 <70779585+retater56@users.noreply.github.com>
2021-02-21 22:22:02 +0300

    feat: create a task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str.length % 2 !==0) {
+        return false;
+      }
+
+      let bracket = [];
+    
+      for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+          if (str[i] == bracketsConfig[j][1] && bracketsConfig[j][0] == bracket[bracket.length - 1]) {
+            bracket.pop();
+            continue;
+          }
+    
+          if (str[i] == bracketsConfig[j][0]) {
+            bracket.push(str[i]);
+          }
+    
+        }
+    
+      }
+      
+      if (bracket.length != 0) {
+        return false;
+      }
+    
+      return true;
 }
